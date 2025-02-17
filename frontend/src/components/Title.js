17bae38e7d3a9a81474b7f0091dec6cd59b86a1b
Bahageel88 <bhageel1988@gmail.com>
2025-02-17 12:02:54 +0300

    solving the orb issue

--- frontend/src/components/Title.js
+++ frontend/src/components/Title.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/Title.css';
 
 const Title = ({ pageIndex }) => {
-  const titles = [' RAG Voice Assistant ', 'Call The AI Agent', 'Book An Appointment'];
+  const titles = ['  PATIENT VOICE ASSISTANT ', 'Call The AI Agent', 'Book An Appointment'];
 
   return (
     <h1 className='title'>{titles[pageIndex]}</h1>
